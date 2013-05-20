c04e8b53a1e980838aa67efd219a7d3af77bafe2
Jonathan Heathcote <mail@jhnet.co.uk>
2013-05-20 14:27:08 +0100

    Added basic wire-length metric.

---
+++ metrics.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+A library of functions which calculate various metrics about systems of boards
+and their wiring.
+"""
+
+import topology
+import coordinates
+import cabinet
+
+
+def wire_length(boards, board, direction, wire_offsets={}):
+	"""
+	Returns the length of a wire leaving the specified board in a given direction.
+	
+	boards is a list [(board, coord),...)] where all coords support subtraction
+	and magnitude() (such as those from the coordinates module).
+	
+	board is a board in that list
+	
+	direction is a wire direction to measure
+	
+	wire_offsets is an (optional) dict {direction:offset,...} where the offset
+	supplied for each direction.
+	"""
+	b2c = dict(boards)
+	source = b2c[board]
+	target = b2c[board.connection[direction]]
+	
+	if direction in wire_offsets:
+		source += wire_offsets[direction]
+	if topology.opposite(direction) in wire_offsets:
+		target += wire_offsets[topology.opposite(direction)]
+	
+	return (source - target).magnitude()
+
--- tests.py
+++ tests.py
@@ -11,13 +11,12 @@ import unittest
 from itertools import product
 import fractions
 
-import board
-
-import cabinet
-
 import topology
-
+import board
+import cabinet
 import coordinates
+import transforms
+import metrics
 
 class TopologyTests(unittest.TestCase):
 	"""
@@ -585,6 +584,54 @@ class CabinetTests(unittest.TestCase):
 		
 		# Check accessing a particular link
 		self.assertEqual(sys.get_position((0,0,2), topology.NORTH), (6.75,3.5,2.0))
+
+
+
+class MetricsTests(unittest.TestCase):
+	"""
+	Tests for the metrics functions
+	"""
+	
+	def test_wire_length(self):
+		# Define *some* port locations
+		wire_offsets = {
+			topology.NORTH : (1,1),
+			topology.SOUTH : (-1,-1),
+		}
+		
+		# Get a single three-board like this
+		#
+		#  (0,2)
+		#        (1,1)
+		#  (0,0)
+		boards = transforms.hex_to_cartesian(board.create_torus(1))
+		
+		c2b = dict((c,b) for (b,c) in boards)
+		
+		# Pick the board at (0,0)
+		b = c2b[(0,0)]
+		
+		# North wire (which reaches the chip above)
+		self.assertTrue(metrics.wire_length(boards, b, topology.NORTH), 2.0)
+		# South wire (which reaches the chip above-right)
+		self.assertTrue(metrics.wire_length(boards, b, topology.SOUTH), 2.0**0.5)
+		
+		# Now while giving a non-applicable wire_offsets
+		# North-East wire (which reaches the chip above-right)
+		self.assertTrue(metrics.wire_length(boards, b, topology.NORTH_EAST, wire_offsets),
+			2.0**0.5)
+		# South-West wire (which reaches the chip above)
+		self.assertTrue(metrics.wire_length(boards, b, topology.SOUTH_WEST, wire_offsets),
+			2.0)
+		
+		# Now while giving a applicable wire_offsets
+		# North wire (which reaches the chip above)
+		self.assertTrue(metrics.wire_length(boards, b, topology.NORTH, wire_offsets),
+			3**0.5)
+		# South wire (which reaches the chip above-right)
+		self.assertTrue(metrics.wire_length(boards, b, topology.SOUTH, wire_offsets),
+			2.0)
+
 
 
 if __name__=="__main__":
