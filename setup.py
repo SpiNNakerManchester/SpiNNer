6db3f3db4946b58c3898112509132243725bf7f8
Jonathan Heathcote <mail@jhnet.co.uk>
2015-05-04 15:53:46 +0100

    Add basic setuptools setup file.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="spinnaker_spinner",
+    version="0.1.2",
+    packages=find_packages(),
+
+    # Metadata for PyPi
+    author="Jonathan Heathcote",
+    description="A tool for generating wiring instructions for large SpiNNaker machines.",
+    license="GPLv2",
+
+    # Requirements
+    install_requires=["six"],
+)
