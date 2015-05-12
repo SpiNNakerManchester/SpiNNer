f86fd686ef179d602bee471ed4b47fd0daac4269
Jonathan Heathcote <mail@jhnet.co.uk>
2015-05-12 12:05:31 +0100

    Add physical wire length function.

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -7,8 +7,11 @@ from spinner import coordinates
 
 from spinner.topology import Direction
 
+from math import pi, sin, cos
 
-def test_wire_length():
+
+def test_wire_lengths():
+	"""Test with a few singleton examples to check the arithmetic."""
 	# Define *some* port locations
 	wire_offsets = {
 		Direction.north_east : (0, 0, 0),
@@ -26,43 +29,45 @@ def test_wire_length():
 	#  (0,0)
 	boards = transforms.hex_to_cartesian(board.create_torus(1))
 	
-	c2b = dict((c,b) for (b,c) in boards)
-	
-	# Pick the board at (0,0)
-	b = c2b[(0,0)]
+	# Place board (0, 0) at the start of the list as this is the board we're
+	# interested in below.
+	for b, c in boards:
+		if c == (0, 0):
+			boards.remove((b, c))
+	boards.insert(0, (b, c))
 	
 	# North wire (which reaches the chip above)
-	assert metrics.wire_length(boards, b, Direction.north) == 2.0
+	assert list(metrics.wire_lengths(boards, Direction.north))[0] == 2.0
 	# South wire (which reaches the chip above-right)
-	assert metrics.wire_length(boards, b, Direction.south) == 2.0**0.5
+	assert list(metrics.wire_lengths(boards, Direction.south))[0] == 2.0**0.5
 	
 	# Now while giving a non-applicable wire_offsets
 	# North-East wire (which reaches the chip above-right)
-	assert metrics.wire_length(boards, b, Direction.north_east, wire_offsets) == \
+	assert list(metrics.wire_lengths(boards, Direction.north_east, wire_offsets))[0] == \
 		2.0**0.5
 	# South-West wire (which reaches the chip above)
-	assert metrics.wire_length(boards, b, Direction.south_west, wire_offsets) == \
+	assert list(metrics.wire_lengths(boards, Direction.south_west, wire_offsets))[0] == \
 		2.0
 	
 	# Now while giving a applicable wire_offsets
 	# North wire (which reaches the chip above)
-	assert metrics.wire_length(boards, b, Direction.north, wire_offsets) == \
+	assert list(metrics.wire_lengths(boards, Direction.north, wire_offsets))[0] == \
 		2.0
 	# South wire (which reaches the chip above-right)
-	assert metrics.wire_length(boards, b, Direction.south, wire_offsets) == \
+	assert list(metrics.wire_lengths(boards, Direction.south, wire_offsets))[0] == \
 		18**0.5
 	
 	# Test with 3D coordinates (just sets z to x+y)
 	boards_3D = [(b, coordinates.Cartesian3D(c[0], c[1], c[0]+c[1]))
 	             for (b, c) in boards]
-	c2b_3D = dict((c,b) for (b,c) in boards_3D)
-	metrics.wire_length(boards_3D, c2b_3D[(0,0,0)], Direction.north) == \
+	assert list(metrics.wire_lengths(boards_3D, Direction.north))[0] == \
 		8**0.5
-	metrics.wire_length(boards_3D, c2b_3D[(0,0,0)], Direction.north, wire_offsets) == \
+	assert list(metrics.wire_lengths(boards_3D, Direction.north, wire_offsets))[0] == \
 		8**0.5
 
 
-def test_wire_lengths():
+def test_wire_lengths_multiple():
+	"""Test multiple boards can be fed into the wire_lengths function."""
 	# Get a single three-board like this
 	#
 	#  (0,2)
@@ -78,6 +83,51 @@ def test_wire_lengths():
 		# 0,2
 		2**0.5,  # North
 	])
+
+
+@pytest.mark.parametrize("distance,minimum_arc_height,"
+                         "exp_wire_length,exp_arc_height",
+                         [# Special case: zero-length
+                          (0.0, 0.0, 0.15, 0.15/2),
+                          # Special case: zero-height arcs allowed, should just
+                          # pick available wire.
+                          (0.15, 0.0, 0.15, 0.0),
+                          (0.30, 0.0, 0.30, 0.0),
+                          (0.50, 0.0, 0.50, 0.0),
+                          (1.00, 0.0, 1.00, 0.0),
+                          # Special case: wire is too long to form an arc,
+                          # should form a semi-circle at a distance instead.
+                          (0.05, 0.0, 0.15, (0.15 - ((0.05*pi)/2))/2 + 0.05/2),
+                         ])
+def test_physical_wire_length_cases(distance, minimum_arc_height,
+                                    exp_wire_length, exp_arc_height):
+	"""Test a few specific special-cases of wire length."""
+	wire_length, arc_height = metrics.physical_wire_length(
+		distance, [0.15, 0.3, 0.5, 1.0], minimum_arc_height)
+	
+	assert wire_length == exp_wire_length
+	assert abs(arc_height - exp_arc_height) < 0.01
+
+
+
+@pytest.mark.parametrize("alpha",
+                         [((n+1) / 30.0) * pi
+                          for n in range(30)])
+def test_physical_wire_length_sweep(alpha):
+	"""Test a value sweep of allowable wire lengths."""
+	exp_wire_length = 1.0
+	
+	# Compute the model answer using straight forward trig
+	distance = 2.0 * (exp_wire_length/alpha) * sin(alpha / 2.0)
+	exp_arc_height = (exp_wire_length/alpha) - (exp_wire_length/alpha) * cos(alpha/2.0)
+	
+	wire_length, arc_height = metrics.physical_wire_length(
+		distance, [exp_wire_length], 0.0)
+	
+	assert wire_length == exp_wire_length
+	assert abs(arc_height - exp_arc_height) < 0.01
+
+
 
 
 def test_wire_length_histogram():
